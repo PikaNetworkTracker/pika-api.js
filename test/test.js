a7da7b5a965ffc2ecd3eef4ec4f00db58770978f
mallusrgreat <mallusrgreat@duck.com>
2024-04-04 14:36:08 +0530

    feat: tests

---
+++ test/test.js
@@ -0,0 +1,41 @@
+const assert = require("node:assert");
+const lib = require("../lib");
+describe("PikaNetwork", () => {
+  const pika = new lib.PikaNetwork();
+  describe("#getCount", () => {
+    it("ip must return play.pika-network.net", async () => {
+      const count = await pika.getCount();
+      assert.equal(count.ip, "play.pika-network.net");
+    });
+  });
+  describe("#getProfileLeaderboard", () => {
+    it("metadata total must be a number", async () => {
+      const obj = await pika.getProfileLeaderboard({
+        gamemode: lib.Gamemode.BedWars,
+        username: "mallusrgreat",
+      });
+      assert.strictEqual(typeof obj.Kills.metadata.total, "number");
+    });
+  });
+  describe("#getProfile", () => {
+    it("username should be mallusrgreat", async () => {
+      const obj = await pika.getProfile("mallusrgreat");
+      assert.strictEqual(obj.username, "mallusrgreat");
+    });
+  });
+  describe("#getLeaderboard", () => {
+    it("bedwars lb should total kills must be a number", async () => {
+      const obj = await pika.getLeaderboard({
+        gamemode: lib.Gamemode.BedWars,
+        leaderboardType: lib.LeaderboardType.Kills,
+      });
+      assert.strictEqual(typeof obj.metadata.total, "number");
+    });
+  });
+  describe("#getTotalLeaderboard", () => {
+    it("bedwars total lb total must be a number", async () => {
+      const obj = await pika.getTotalLeaderboard(lib.Gamemode.BedWars);
+      assert.strictEqual(typeof obj[0].total, "number");
+    });
+  });
+});
